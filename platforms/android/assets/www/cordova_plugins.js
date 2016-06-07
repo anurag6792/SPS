3f55b3b05e977208dca60c2ec5e0617dad761471
anurag6792 <anuragv@dynaptsolutions.com>
2016-06-07 16:36:00 +0530

    762016-part2
    minor changes

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -242,13 +242,13 @@ module.exports.metadata =
     "cordova-plugin-device": "1.1.2",
     "cordova-plugin-console": "1.0.3",
     "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-splashscreen": "3.1.0",
+    "cordova-plugin-splashscreen": "3.2.2",
     "cordova-plugin-statusbar": "2.1.3",
     "ionic-plugin-keyboard": "2.1.0",
     "cordova-plugin-camera": "2.1.1",
     "cordova-plugin-file": "4.1.1",
     "cordova-plugin-network-information": "1.2.1",
     "cordova-plugin-geolocation": "2.1.0"
-};
+}
 // BOTTOM OF METADATA
 });
